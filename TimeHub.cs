fb81ebaa97003beea75765f8a9a3138c29ddb543
clintomed <camedber@g.cofc.edu>
2014-09-10 15:20:43 -0400

    ClockSync HW
    A simple clocksyncer

---
+++ TimeHub.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+
+
+namespace ClockSync
+{
+    public class TimeHub : Hub
+    {
+        public void Sync(string name, float timeInMilliseconds)
+        {
+            DateTime now = DateTime.Now;
+            float newTime = (now.Millisecond + timeInMilliseconds) / 2;
+            //Calling a method to update the client's clocks
+            Clients.All.broadcastTime(name, newTime);
+
+        }
+    }
+}
